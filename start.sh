257c475d5ce736718e8a5fe1fd5d1efb70c15496
Bob Herrmann <bob@jadn.com>
2016-02-03 17:59:22 -0500

    minor setup files

--- start.sh
+++ start.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-cd /home/bob/judge
-lein ring server
+# This script is used to enable a linux box plugged in at the sciencefair
+# become a DNS server on the wifi.   This allows judges to connect with
+# the server and access the judge application
+
+
+# Start
+# Configure IP address for WLAN
+sudo ifconfig wlan0 192.168.150.1
+# Start DHCP/DNS server
+sudo service dnsmasq restart
+# Enable routing
+sudo sysctl net.ipv4.ip_forward=1
+# Enable NAT
+sudo iptables -t nat -A POSTROUTING -o ppp0 -j MASQUERADE
+# Run access point daemon
+sudo hostapd /etc/hostapd.conf
+# Stop
+# Disable NAT
+sudo iptables -D POSTROUTING -t nat -o ppp0 -j MASQUERADE
+# Disable routing
+sudo sysctl net.ipv4.ip_forward=0
+# Disable DHCP/DNS server
+sudo service dnsmasq stop
+sudo service hostapd stop
